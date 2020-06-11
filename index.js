d96940e397bde0ed083b3b58e56985195ad15b22
Phillip Shields <github@noreply>
2020-06-12 11:00:19 +1200

    some more tweaks

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-//
-class GameOfLifeBoard {
-  constructor() {
+function GameOfLifeBoard() {
     this.board = [...Array(10)].map((row) => Array(10));
     this.board.forEach((row) => (row += "\n"));
-  }
 }
 
 const randomFillBoard = (input) => {
@@ -27,21 +24,32 @@ const createUpdatedBoard = (input) => {
 
   for (let i = 0; i < input.board.length; i++) {
     for (let j = 0; j < input.board.length; j++) {
-      // add logic to identify if current cell has value of 1 or 0
       let count = checkEnvOfCell(input, i, j);
-
-      if (input.board[i][j] === 1) {
-        count > 1 && count < 4 ? (newBoard[i][j] = 1) : (newBoard[i][j] = 0);
-      } else if (input.board[i][j] === 0 && count === 3) {
-        newBoard[i][j] = 1;
-      } else {
-        newBoard[i][j] = 0;
-      }
+      // if (isAlive(input, i, j)) newBoard[i][j] = fillSquare(count);
+      // else if (isRebirth(input,i,j,count)) newBoard[i][j] = 1;
+      // else newBoard[i][j] = 0;
+      newBoard = fillNewBoard(count,input,newBoard,i,j);
     }
   }
   return (input.board = newBoard);
 };
 
+const fillNewBoard = (count, input, newBoard, i, j) => {
+  if (input.board[i][j] === 1) {
+    newBoard[i][j] = fillSquare(count);
+  } else if (input.board[i][j] === 0 && count === 3) {
+    newBoard[i][j] = 1;
+  } else {
+    newBoard[i][j] = 0;
+  }
+  return newBoard;
+};
+
+// const isAlive = (input,i,j) => (input.board[i][j] === 1); 
+// const isRebirth = (input,i,j,count) => (input.board[i][j] === 0 && count === 3);
+const fillSquare = (count) => {
+  return count > 1 && count < 4 ? 1 : 0;
+};
 const checkEnvOfCell = (input, i, j) => {
   let count = 0;
   const env = [
